2d22c647aeb3bf5548b082ad6f31bd83ab98f347
Vildar Khisyametdinov <vildar82@gmail.com>
2016-12-16 13:15:21 +0300

    Уровни освещенности - DoubleConverter

--- PIK_GP_Acad/Properties/AssemblyInfo.cs
+++ PIK_GP_Acad/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номера сборки и редакции по умолчанию 
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.351.216")]
-[assembly: AssemblyFileVersion("1.0.351.216")]
+[assembly: AssemblyVersion("1.0.351.217")]
+[assembly: AssemblyFileVersion("1.0.351.217")]
